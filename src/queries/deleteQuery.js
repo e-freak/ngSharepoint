f8710dc124e5261a3158843f739c2d2bde03758c
Max Joehnk <maxjoehnk@gmail.com>
2015-10-23 10:01:46 +0200

    Implemented DeleteQuery

--- src/queries/deleteQuery.js
+++ src/queries/deleteQuery.js
@@ -2,25 +2,51 @@ angular
 	.module('ngSharepoint')
 	.factory('DeleteQuery', ['$q', '$sp', 'WhereQuery', 'Query', function($q, $sp, WhereQuery, Query) {
 		var DeleteQuery = function() {
+            this.__where = null;
+            this.__list = null;
 			return this;
 		};
+        DeleteQuery.prototype = new Query();
         DeleteQuery.prototype.from = function(list) {
-            
+            this.__list = list;
             return this;
         };
-        DeleteQuery.prototype.join = function(list) {
-            return {
-                __query: this,
-                on: function(row1, row2) {
-
-                    return __query;
-                };
-            };
-        }
-		DeleteQuery.prototype = new Query();
+        DeleteQuery.prototype.where = function(key) {
+            this.__where = new WhereQuery(this, key);
+            return this.__where;
+        };
 		DeleteQuery.prototype.__execute = function() {
             var query = this;
             return $q(function(resolve, reject) {
+                var clientContext = $sp.getContext();
+                var list = clientContext.get_web().get_lists().getByTitle(query.__list);
+                var camlQuery = new SP.CamlQuery();
+                var caml = ['<View>'];
+                if (query.__where !== null) {
+                    query.__where.push(caml);
+                }
+                caml.push('</View>');
+                camlQuery.set_viewXml(caml.join(''));
+                var items = list.getItems(camlQuery);
+                clientContext.load(items);
+                clientContext.executeQueryAsync(
+                    function(sender, args) {
+                        var itemIterator = items.getEnumerator();
+                        while (itemIterator.moveNext()) {
+                           var item = itemIterator.get_current();
+                           item.deleteObject();
+                        }
+                        clientContext.executeQueryAsync(function(sender, args) {
+                            resolve(args);
+                        }, function(sender, args) {
+                            reject(args);
+                        });
+                    },
+                    function(sender, args) {
+                        reject(args);
+                    }
+                );
             });
         };
+        return (DeleteQuery);
 	}]);
