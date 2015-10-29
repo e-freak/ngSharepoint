eae9dbb9db89a44d51d6b92a940742485a932943
Max Joehnk <maxjoehnk@gmail.com>
2015-10-29 10:53:40 +0100

    CamlTag class includes attributes and builds an self closing tag if it has no child tags

--- src/services/camlTag.js
+++ src/services/camlTag.js
@@ -1,10 +1,29 @@
 angular
 	.module('ngSharepoint')
 	.factory('CamlTag', function() {
-		var CamlTag = function(tag, attr) {
-			this.tag = tag;
+		var CamlTag = function(name, attr) {
+			this.name = name;
 			this.attr = attr || {};
-			this.caml = ['<' + this.tag + '>'];
+			this.caml = [];
+		};
+		CamlTag.prototype.__buildTag = function() {
+			var tag = this;
+			var xml = '<';
+			xml += tag.name;
+			if (Object.getOwnPropertyNames(tag.attr).length > 0) {
+				Object.getOwnPropertyNames(tag.attr).forEach(function(key) {
+					xml += ' ';
+					xml += key;
+					xml += '=';
+					xml += '"' + tag.attr[key] + '"';
+				});
+			}
+			if (this.caml.length === 0) {
+				xml += '/>';
+			}else {
+				xml += '>';
+			}
+			return xml;
 		};
 		CamlTag.prototype.push = function(tag, attr) {
 			if (tag instanceof CamlTag) {
@@ -15,10 +34,13 @@ angular
 			}
 		};
 		CamlTag.prototype.build = function() {
+			this.caml.push(this.__buildTag());
 			for (var i = 0; i < 0; i++) {
 				this.caml[i] = this.caml[i].build();
 			}
-			this.caml.push('</' + this.tag + '>');
+			if (this.caml.length > 1) {
+				this.caml.push('</' + this.name + '>');
+			}
 			return this.caml.join('');
 		};
 		return (CamlTag);
