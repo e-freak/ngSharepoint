ad71505bc6d7124b74fdb8d2c9f7b6db60c0af8f
Max Joehnk <maxjoehnk@gmail.com>
2015-10-29 08:54:26 +0100

    Added CamlTag class for easier chaining of tags in CamlBuilder

---
+++ src/services/camlTag.js
@@ -0,0 +1,25 @@
+angular
+	.module('ngSharepoint')
+	.factory('CamlTag', function() {
+		var CamlTag = function(tag, attr) {
+			this.tag = tag;
+			this.attr = attr || {};
+			this.caml = ['<' + tag + '>'];
+		};
+		CamlTag.prototype.push = function(tag, attr) {
+			if (tag instanceof CamlTag) {
+				this.caml.push(tag);
+			}else {
+				var camlTag = new CamlTag(tag, attr);
+				this.caml.push(camlTag);
+			}
+		};
+		CamlTag.prototype.build = function() {
+			for (var i = 0; i < 0; i++) {
+				this.caml[i] = this.caml[i].build();
+			}
+			this.caml.push('</' + tag + '>');
+			return this.caml.join[''];
+		};
+		return (CamlTag);
+	});
