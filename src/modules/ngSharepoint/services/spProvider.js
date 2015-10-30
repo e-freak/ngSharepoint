c81f7515e062ebcd24336abd99a86d8a9fca4081
Max Joehnk <maxjoehnk@gmail.com>
2015-10-30 07:50:52 +0100

    New Code Structure, splitting functionality into modules

--- src/modules/ngSharepoint/services/spProvider.js
+++ src/modules/ngSharepoint/services/spProvider.js
@@ -2,9 +2,15 @@ angular
 	.module('ngSharepoint')
 	.provider('$sp', function() {
 		var siteUrl = "";
+		var connMode = "JSOM"; //possible values: JSOM, REST
 		return {
 			setSiteUrl: function (newUrl) {
 				siteUrl = newUrl;
+			},
+			setConnectionMode: function(connMode) {
+				if (connMode == "JSOM" || connMode == "REST") {
+					this.connMode = connMode;
+				}
 			},
 			$get: function() {
 				return ({
