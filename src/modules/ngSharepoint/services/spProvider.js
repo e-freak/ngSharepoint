9112e1a6200b045b3550f9af85f7cc2d7fa416e1
Max Joehnk <maxjoehnk@gmail.com>
2015-11-06 09:53:23 +0100

    Added connectionmode getter

--- src/modules/ngSharepoint/services/spProvider.js
+++ src/modules/ngSharepoint/services/spProvider.js
@@ -7,7 +7,7 @@ angular
 			setSiteUrl: function (newUrl) {
 				siteUrl = newUrl;
 			},
-			setConnectionMode: function(connMode) {
+			setConnectionMode: function(connMode) { //Only JSOM Supported for now
 				if (connMode == "JSOM" || connMode == "REST") {
 					this.connMode = connMode;
 				}
@@ -16,6 +16,9 @@ angular
 				return ({
 					getSiteUrl: function() {
 						return siteUrl;
+					},
+					getConnectionMode: function() {
+						return connMode;
 					},
 					getContext: function() {
 						return new SP.ClientContext(siteUrl);
