dd43c0ffd08ce98fd38835cf989cad3bf2e93dab
Max Joehnk <maxjoehnk@gmail.com>
2015-11-11 09:28:56 +0100

    Use $spCamlParser to parse incoming queries

--- src/modules/ngSharepointMocks/mocks/lists/spList.js
+++ src/modules/ngSharepointMocks/mocks/lists/spList.js
@@ -1,6 +1,6 @@
 angular
 	.module('ngSharepoint.Mocks')
-	.factory('SPList', ['lists', function(lists) {
+	.factory('SPList', ['$spCamlParser', 'lists', function($spCamlParser, lists) {
 		var SPList = function(title) {
 			this.title = title;
 			this.list = lists[title]; //{cols: [], data:[[]]}
@@ -13,15 +13,83 @@ angular
 					alignedData[i] = data[list.list.cols[i]];
 				}
 				this.list.data.push(alignedData);
-				resolve();
+				resolve(data);
 			});
 		};
 		SPList.prototype.select = function(query) {
 			var list = this;
 			return $q(function(resolve, reject) {
 				var data = [];
-				var parser = new DOMParser();
-				camlQuery = parser.parseFromString(query, 'text/xml');
+				var result = [];
+				var parser = $spCamlParser.parse(query);
+				if (parser.hasWhere()) {
+					var where = parser.getWhere();
+					for (var i = 0; i < list.list.data.length; i++) {
+						var row = list.list.data[i];
+						if (list.__filter(where, row)) {
+							data.push(row);
+						}
+					}
+				}else {
+					data = list.list.data;
+				}
+				if (parser.hasViewFields()) {
+					var cols = [];
+					parser.getViewFields().forEach(function(field) {
+						var colId = list.list.cols.indexOf(field);
+						if (colId === -1) {
+							reject('Invalid column name');
+						}else {
+							cols.push(colId);
+						}
+					});
+					result = [];
+					data.forEach(function(row) {
+						var entry = {};
+						for (var i = 0; i < row.length; i++) {
+							if (cols.indexOf(i) !== -1) {
+								entry[list.list.cols[i]] = row[i];
+							}
+						}
+						result.push(entry);
+					});
+					data = result;
+				}else {
+					result = [];
+					data.forEach(function(row) {
+						var entry = {};
+						for (var i = 0; i < row.length; i++) {
+							if (list.list.cols.indexOf(i) !== -1) {
+								entry[list.list.cols[i]] = row[i];
+							}
+						}
+						result.push(entry);
+					});
+					data = result;
+				}
+				if (parser.hasLimit()) {
+					result = [];
+					for (var k = 0; k < parser.geLimit(); k++) {
+						result.push(data[k]);
+					}
+					data = result;
+				}
+			});
+		};
+		SPList.prototype.delete = function(query) {
+			return $q(function(resolve, reject) {
+
+			});
+		};
+		SPList.prototype.update = function(query, data) {
+			return $q(function(resolve, reject) {
+
+			});
+		};
+		SPList.prototype.__filter = function(row, query) {
+			//TODO: Implement
+			return true;
+							/*camlQuery = parser.parseFromString(query, 'text/xml');
 				var queryTag = camlQuery.getElementsByTagName('Query')[0];
 				if (queryTag !== null) {
 					var equalTags = queryTag.getElementsByTagName('Eq');
@@ -50,46 +118,6 @@ angular
 					}
 				}else {
 					data = list.list.data;
-				}
-				var viewFieldsTag = camlQuery.getElementsByTagName('ViewFields')[0];
-				if (viewFieldsTag !== null) {
-					var cols = [];
-					viewFieldsTag.getElementsByTagName('FieldRef').forEach(function(field) {
-						var colId = list.list.cols.indexOf(field.attributes.Name.value);
-						if (colId === -1) {
-							reject('Invalid column name');
-						}else {
-							cols.push(colId);
-						}
-					});
-					data.forEach(function(row) {
-						var entry = [];
-						for (var i = 0; i < row.length; i++) {
-							if (cols.indexOf(i) !== -1) {
-
-							}
-						}
-					});
-				}
-				var rowLimitTag = camlQuery.getElementsByTagName('RowLimit')[0];
-				if (rowLimitTag !== null) {
-					var limit = rowLimitTag.nodeValue;
-					var result = [];
-					for (var k = 0; k < limit; k++) {
-						result.push(data[k]);
-					}
-					data = result;
-				}
-			});
-		};
-		SPList.prototype.delete = function(query) {
-			return $q(function(resolve, reject) {
-
-			});
-		};
-		SPList.prototype.update = function(query, data) {
-			return $q(function(resolve, reject) {
-
-			});
+				}*/
 		};
 	}]);
