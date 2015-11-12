dafbdc9abff9d3fa87460deb650d210d85e8fd76
Max Joehnk <maxjoehnk@gmail.com>
2015-11-12 21:49:57 +0100

    Wait for Library to load before accessing it

--- src/modules/ngSharepointUsers/services/spUser.js
+++ src/modules/ngSharepointUsers/services/spUser.js
@@ -37,20 +37,22 @@ angular
         JsomSPUser.prototype.load = function() {
             var user = this;
             return $q(function(resolve, reject) {
-                var context = $sp.getContext();
-                var peopleManager = new SP.UserProfiles.PeopleManager(context);
-                var properties = peopleManager.getPropertiesFor(user.accountName);
-                context.load(properties);
-                context.executeQueryAsync(function() {
-                    var data = {};
-                    data.displayName = properties.get_displayName();
-                    data.email = properties.get_email();
-                    data.picture = properties.get_pictureUrl();
-                    data.title = properties.get_title();
-                    data.personalUrl = properties.get_personalUrl();
-                    data.userUrl = properties.get_userUrl();
-                    resolve(data);
-                }, reject);
+                $spLoader.waitUntil('SP.Userprofiles.js').then(function() {
+                    var context = $sp.getContext();
+                    var peopleManager = new SP.UserProfiles.PeopleManager(context);
+                    var properties = peopleManager.getPropertiesFor(user.accountName);
+                    context.load(properties);
+                    context.executeQueryAsync(function() {
+                        var data = {};
+                        data.displayName = properties.get_displayName();
+                        data.email = properties.get_email();
+                        data.picture = properties.get_pictureUrl();
+                        data.title = properties.get_title();
+                        data.personalUrl = properties.get_personalUrl();
+                        data.userUrl = properties.get_userUrl();
+                        resolve(data);
+                    }, reject);
+                });
             });
         };
         var RestSPUser = function(accountName) {
