6bbb177c5f82400062df485510c1143f6ec72603
Max Joehnk <maxjoehnk@gmail.com>
2015-11-12 19:03:36 +0100

    Autoload Sharepoint Librarys if needed

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -1,2 +1,10 @@
 angular
-	.module('ngSharepoint', []);
+    .module('ngSharepoint', [])
+    .run(['$sp', '$spLoader', function($sp, $spLoader) {
+        if ($sp.getAutoload()) {
+            if ($sp.getConnectionMode() === 'JSOM') {
+                $spLoader.load($sp.getSiteUrl() + '/_layouts/15/SP.Runtime.js');
+                $spLoader.load($sp.getSiteUrl() + '/_layouts/15/SP.js');
+            }
+        }
+    }]);
--- src/modules/ngSharepointUsers/app.js
+++ src/modules/ngSharepointUsers/app.js
@@ -1,2 +1,9 @@
 angular
-  .module('ngSharepoint.Users', ['ngSharepoint']);
+    .module('ngSharepoint.Users', ['ngSharepoint'])
+    .run(['$sp', '$spLoader', function($sp, $spLoader) {
+        if ($sp.getAutoload()) {
+            if ($sp.getConnectionMode() === 'JSOM') {
+                $spLoader.load($sp.getSiteUrl() + '/_layouts/15/SP.Userprofiles.js');
+            }
+        }
+    }]);
