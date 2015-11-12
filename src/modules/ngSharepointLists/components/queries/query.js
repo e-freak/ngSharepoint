ebdc564d5a65bacdce80943539abd5a46cfe2a36
Max Joehnk <maxjoehnk@gmail.com>
2015-11-12 12:54:36 +0100

    Moved unpackItem/packItem and __trim to the actual list implementation

--- src/modules/ngSharepointLists/components/queries/query.js
+++ src/modules/ngSharepointLists/components/queries/query.js
@@ -4,37 +4,8 @@ angular
         var Query = function() {
             //this.__queries = {};
         };
-        Query.prototype.unpackItem = function(item) {
-            var query = this;
-            var obj = {};
-            if (Array.isArray(query.__values)) {
-                query.__values.forEach(function(key) {
-                    var value = item.get_item(key);
-                    obj[key] = query.__trim(value);
-                });                
-            }else {
-                Object.getOwnPropertyNames(query.__values).forEach(function(key) {
-                    var value = item.get_item(key);
-                    obj[key] = query.__trim(value);
-                });
-            }
-            return obj;
-        };
-        Query.prototype.packItem = function(item) {
-            var query = this;
-            Object.getOwnPropertyNames(query.__values).forEach(function(key) {
-                item.set_item(key, query.__trim(query.__values[key]));
-            });
-        };
         Query.prototype.then = function(success, reject) {
             return this.__execute().then(success, reject);
-        };
-        Query.prototype.__trim = function(value) {
-            if (value !== null && value !== undefined && typeof value === 'string') {
-                return value.trim();
-            }else {
-                return value;
-            }
         };
         return (Query);
     });
