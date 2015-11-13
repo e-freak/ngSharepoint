3a427108bd539987dbd3847edee623cf777b40ad
Max Joehnk <maxjoehnk@gmail.com>
2015-11-13 10:14:30 +0100

    Load Ajax Library before loading sharepoint libraries

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -3,8 +3,9 @@ angular
     .run(['$sp', '$spLoader', function($sp, $spLoader) {
         if ($sp.getAutoload()) {
             if ($sp.getConnectionMode() === 'JSOM') {
+                $spLoader.loadScript('//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js');
                 $spLoader.loadScript('SP.Runtime.js');
-                $spLoader.loadScript('SP.js');
+                $spLoader.loadScript('SP.js');                    
             }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken) {
             	$spLoader.loadScript('SP.RequestExecutor.js');
             }
