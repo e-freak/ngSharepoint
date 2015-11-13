70be3f4cc8a091da46e886123c7f0f8cb43db9e0
Max Joehnk <maxjoehnk@gmail.com>
2015-11-13 12:12:33 +0100

    Load main JSOM Scripts under one label

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -3,9 +3,7 @@ angular
     .run(['$sp', '$spLoader', function($sp, $spLoader) {
         if ($sp.getAutoload()) {
             if ($sp.getConnectionMode() === 'JSOM') {
-                $spLoader.loadScript('//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js');
-                $spLoader.loadScript('SP.Runtime.js');
-                $spLoader.loadScript('SP.js');                    
+                $spLoader.loadScripts('SP.Core', ['//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js', 'SP.Runtime.js', 'SP.js']);                    
             }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken) {
             	$spLoader.loadScript('SP.RequestExecutor.js');
             }
