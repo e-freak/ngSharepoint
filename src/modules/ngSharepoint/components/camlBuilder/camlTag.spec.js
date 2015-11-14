b18f1d99d52367c2f3fb3593598121450429495c
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 11:42:48 +0100

    Use only single quotes

--- src/modules/ngSharepoint/components/camlBuilder/camlTag.spec.js
+++ src/modules/ngSharepoint/components/camlBuilder/camlTag.spec.js
@@ -8,7 +8,7 @@ describe('The CamlTag should build a valid XML String', function() {
 		expect(new CamlTag('Test').build()).toEqual('<Test/>');
 	});
 	it('builds a valid XML Tag with Attributes', function() {
-		expect(new CamlTag('Test', {Type: "Test"}).build()).toEqual('<Test Type="Test"/>');
+		expect(new CamlTag('Test', {Type: 'Test'}).build()).toEqual('<Test Type="Test"/>');
 	});
 	it('builds a valid XML Hierarchy', function() {
 		var tag = new CamlTag('Test');
