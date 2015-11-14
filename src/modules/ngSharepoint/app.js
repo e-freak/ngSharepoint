4516a7ced770f11f0febe8aec0615ddbb8f6674b
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 17:38:40 +0100

    Define scripts in a array instead of inline

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -3,7 +3,12 @@ angular
     .run(function($sp, $spLoader) {
         if ($sp.getAutoload()) {
             if ($sp.getConnectionMode() === 'JSOM') {
-                $spLoader.loadScripts('SP.Core', ['//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js', 'SP.Runtime.js', 'SP.js']);                    
+                var scripts = [
+                    '//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js',
+                    'SP.Runtime.js',
+                    'SP.js'
+                ];
+                $spLoader.loadScripts('SP.Core', scripts);                    
             }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken()) {
                 $spLoader.loadScript('SP.RequestExecutor.js');
             }
