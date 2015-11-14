03406fea33160b1dd555eff2b72e72c96f742fe6
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 21:00:03 +0100

    Added jscs and load module.js files first

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,15 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-eslint');
+	grunt.loadNpmTasks('grunt-jscs');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-karma');
 	grunt.loadNpmTasks('grunt-ng-annotate');
 
 	grunt.registerTask('default', ['jshint', 'concat:dev', 'clean:coverage', 'karma:dist']);
-	grunt.registerTask('dist', ['jshint', 'eslint', 'clean:coverage', 'karma:dist', 'concat:dist', 'ngAnnotate:dist', 'uglify:dist', 'clean:build']);
-	grunt.registerTask('test', ['jshint', 'eslint', 'clean:coverage', 'karma:dev']);
+	grunt.registerTask('dist', ['jshint', 'eslint', 'jscs', 'clean:coverage', 'karma:dist', 'concat:dist', 'ngAnnotate:dist', 'uglify:dist', 'clean:build']);
+	grunt.registerTask('test', ['jshint', 'eslint', 'jscs', 'clean:coverage', 'karma:dev']);
 
 	grunt.initConfig({
 		concat: {
@@ -20,10 +21,10 @@ module.exports = function(grunt) {
 			dist: {
 				files: {
 					'tmp/angular-sharepoint-full.js':['src/*.js', 'src/**/*.js', '!src/**/*.spec.js', '!src/modules/ngSharepointMocks/**/*.js'],
-					'tmp/angular-sharepoint.js':['src/modules/ngSharepoint/**/*.js', '!src/modules/ngSharepoint/**/*.spec.js'],
-					'tmp/angular-sharepoint-lists.js':['src/modules/ngSharepointLists/**/*.js'],
-					'tmp/angular-sharepoint-users.js':['src/modules/ngSharepointUsers/**/*.js'],
-					'tmp/angular-sharepoint-mocks.js':['src/modules/ngSharepointMocks/**/*.js']
+					'tmp/angular-sharepoint.js':['src/modules/core/*.module.js', 'src/modules/core/**/*.js', '!src/modules/core/**/*.spec.js'],
+					'tmp/angular-sharepoint-lists.js':['src/modules/lists/*.module.js', 'src/modules/ngSharepointLists/**/*.js'],
+					'tmp/angular-sharepoint-users.js':['src/modules/users/*.module.js', 'src/modules/ngSharepointUsers/**/*.js'],
+					'tmp/angular-sharepoint-mocks.js':['src/modules/mocks/*.module.js', 'src/modules/ngSharepointMocks/**/*.js']
 				}
 			}
 		},
@@ -36,6 +37,12 @@ module.exports = function(grunt) {
 		eslint: {
 			options: {
 				configFile: '.eslintrc'
+			},
+			default: ['src/**/*.js', '!src/**/*.spec.js']
+		},
+		jscs: {
+			options: {
+				config: true
 			},
 			default: ['src/**/*.js', '!src/**/*.spec.js']
 		},
@@ -71,6 +78,7 @@ module.exports = function(grunt) {
 				files: [
 					'bower_components/angular/angular.min.js',
 					'bower_components/angular-mocks/angular-mocks.js',
+					'src/**/*.module.js',
 					'src/**/*.js'
 				],
 				singleRun: true,
