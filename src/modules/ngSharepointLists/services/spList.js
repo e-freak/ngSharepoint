a00d63abc125baf5f200735fedb4fbdaa717489e
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 14:35:03 +0100

    Added a getter for all lists

--- src/modules/ngSharepointLists/services/spList.js
+++ src/modules/ngSharepointLists/services/spList.js
@@ -1,9 +1,24 @@
 angular
     .module('ngSharepoint.Lists')
-    .factory('$spList', ['SPList', 'SelectQuery', 'UpdateQuery', 'InsertIntoQuery', 'DeleteQuery', function (SPList, SelectQuery, UpdateQuery, InsertIntoQuery, DeleteQuery) {
+    .factory('$spList', ['$sp', 'SPList', 'JSOMConnector', 'RESTConnector', 'SelectQuery', 'UpdateQuery', 'InsertIntoQuery', 'DeleteQuery', function ($sp, SPList, JSOMConnector, RESTConnector, SelectQuery, UpdateQuery, InsertIntoQuery, DeleteQuery) {
         return ({
             getList: function(title) {
-              return new SPList(title);
+                return new SPList(title);
+            },
+            getLists: function() {
+                var promise;
+                if ($sp.getConnectionMode() === 'JSOM') {
+                    promise = JSOMConnector.getLists();
+                }else {
+                    promise = RESTConnector.getLists();
+                }
+                return promise.then(function(listNames) {
+                    var lists = [];
+                    listNames.forEach(function(name) {
+                        lists.push(new SPList(name));
+                    });
+                    return lists;
+                });
             },
             select: function(fields) {
                 return new SelectQuery(fields);
