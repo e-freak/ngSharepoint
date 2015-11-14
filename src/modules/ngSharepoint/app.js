1016ed83693b55548828e1e03e13f27251ce35c6
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 14:46:35 +0100

    We're accessing a method here

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -4,7 +4,7 @@ angular
         if ($sp.getAutoload()) {
             if ($sp.getConnectionMode() === 'JSOM') {
                 $spLoader.loadScripts('SP.Core', ['//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js', 'SP.Runtime.js', 'SP.js']);                    
-            }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken) {
+            }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken()) {
             	$spLoader.loadScript('SP.RequestExecutor.js');
             }
         }
