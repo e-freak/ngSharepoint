10270383292430299a6398c88e157750e477b2ff
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 12:10:13 +0100

    $spUser is a factory now

---
+++ src/modules/ngSharepointUsers/services/spUser.js
@@ -0,0 +1,21 @@
+angular
+    .module('ngSharepoint.Users')
+    .factory('$spUser', ['$q', '$sp', 'SPUser', function($q, $sp, SPUser) {
+        return({
+            getCurrentUser: function() {
+                //TODO: Abstract with SPUser
+                return $q(function(resolve, reject) {
+                    var context = $sp.getContext();
+                    var peopleManager = new SP.UserProfiles.PeopleManager(context);
+                    var properties = peopleManager.getMyProperties();
+                    context.load(properties);
+                    context.executeQueryAsync(function() {
+                        resolve(properties);
+                    }, reject);
+                });
+            },
+            getUser: function(accountName) {
+                return new SPUser(accountName);
+            }
+        });
+    }]);
