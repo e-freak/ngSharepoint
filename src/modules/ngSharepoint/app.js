90a700dfb8ac5d8f8f02459cf7d2535ca2f63e6e
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 15:00:12 +0100

    Spaceeees

--- src/modules/ngSharepoint/app.js
+++ src/modules/ngSharepoint/app.js
@@ -5,7 +5,7 @@ angular
             if ($sp.getConnectionMode() === 'JSOM') {
                 $spLoader.loadScripts('SP.Core', ['//ajax.aspnetcdn.com/ajax/4.0/1/MicrosoftAjax.js', 'SP.Runtime.js', 'SP.js']);                    
             }else if ($sp.getConnectionMode() === 'REST' && !$sp.getAccessToken()) {
-            	$spLoader.loadScript('SP.RequestExecutor.js');
+                $spLoader.loadScript('SP.RequestExecutor.js');
             }
         }
     }]);
