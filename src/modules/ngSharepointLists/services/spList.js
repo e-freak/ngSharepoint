7823c92724e80a91d62fa7109f7279621207d38c
Max Joehnk <maxjoehnk@gmail.com>
2015-11-14 17:53:58 +0100

    Removed direct query methods and use named functions

--- src/modules/ngSharepointLists/services/spList.js
+++ src/modules/ngSharepointLists/services/spList.js
@@ -1,36 +1,31 @@
 angular
     .module('ngSharepoint.Lists')
-    .factory('$spList', function ($sp, SPList, JSOMConnector, RESTConnector, SelectQuery, UpdateQuery, InsertIntoQuery, DeleteQuery) {
-        return ({
-            getList: function(title) {
-                return new SPList(title);
-            },
-            getLists: function() {
-                var promise;
-                if ($sp.getConnectionMode() === 'JSOM') {
-                    promise = JSOMConnector.getLists();
-                }else {
-                    promise = RESTConnector.getLists();
-                }
-                return promise.then(function(listNames) {
-                    var lists = [];
-                    listNames.forEach(function(name) {
-                        lists.push(new SPList(name));
-                    });
-                    return lists;
-                });
-            },
-            select: function(fields) {
-                return new SelectQuery(fields);
-            },
-            update: function(list) {
-                return new UpdateQuery(list);
-            },
-            insertInto: function(list) {
-                return new InsertIntoQuery(list);
-            },
-            delete: function() {
-                return new DeleteQuery();
-            }
+    .factory('$spList', $spList);
+
+function $spList ($sp, SPList, JSOMConnector, RESTConnector) {
+    var service = {
+        getList: getList,
+        getLists: getLists
+    };
+
+    return service;
+
+    function getList(title) {
+        return new SPList(title);
+    }
+    function getLists() {
+        var promise;
+        if ($sp.getConnectionMode() === 'JSOM') {
+            promise = JSOMConnector.getLists();
+        }else {
+            promise = RESTConnector.getLists();
+        }
+        return promise.then(function(listNames) {
+            var lists = [];
+            listNames.forEach(function(name) {
+                lists.push(new SPList(name));
+            });
+            return lists;
         });
-    });
+    }
+}
